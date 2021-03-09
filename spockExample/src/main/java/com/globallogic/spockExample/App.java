6ae984837b1241b309f223b2966aed508a7646d7
Juan Göttling <31071051+jgottling@users.noreply.github.com>
2021-03-09 16:11:17 -0300

    added micronaut

--- spockExample/src/main/java/com/globallogic/spockExample/App.java
+++ spockExample/src/main/java/com/globallogic/spockExample/App.java
@@ -1,7 +1,9 @@
 package com.globallogic.spockExample;
 
+import io.micronaut.runtime.Micronaut;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Micronaut.run(App.class, args);
     }
 }
